20fbba03b9e3098339f85d131be9bf656fee9116
Andrei Siniavskii <siniavskij.andrei@yandex.ru>
2023-10-02 18:16:41 +0300

    testing models and migrations

--- src/simple_messenger/chats/models.py
+++ src/simple_messenger/chats/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Message(models.Model):
+    sent_at = models.DateTimeField(default=timezone.now)
+    text = models.CharField(max_length=512)
