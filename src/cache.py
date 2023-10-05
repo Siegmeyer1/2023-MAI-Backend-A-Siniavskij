0fdb940ae21d62acfa414866bc0dcbf0efb41273
Andrei Siniavskii <siniavskij.andrei@yandex.ru>
2023-10-05 23:43:27 +0300

    fixed LRU cache (it was FIFO 0_0)

--- src/cache.py
+++ src/cache.py
@@ -4,10 +4,17 @@ class LRUCache:
         self._storage = {}
 
     def get(self, key: str) -> str:
-        return self._storage.get(key) or ''
+        ret_val = self._storage.get(key)
+        if ret_val:
+            del self._storage[key]
+            self._storage[key] = ret_val
+            return ret_val
+        return ''
 
     def set(self, key: str, value: str) -> None:
-        if len(self._storage) >= self.capacity and key not in self._storage:
+        if key in self._storage:
+            del self._storage[key]
+        if len(self._storage) >= self.capacity:
             oldest_key = next(iter(self._storage))
             del self._storage[oldest_key]
         
